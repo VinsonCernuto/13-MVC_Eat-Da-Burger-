7cfb9a6089d745e7931eb684914d2ac7516d762f
Vinson Cernuto <cernutovinson@gmail.com>
2020-10-20 14:04:40 -0400

    added db

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+use burgers_db;
+insert into burgers (burger_name)
+values ("Classic Bacon and Cheese Burger");
+insert into burgers (burger_name)
+values ("BBQ Bacon Western Burger");
+insert into burgers (burger_name)
+values ("Bacon and blue cheese-stuffed burger");
+insert into burgers (burger_name)
+values ("The Classic Burger");
+insert into burgers (burger_name)
+values ("Cheddar-and-Onion Smashed Burger");
